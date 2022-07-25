99764c443f22f56778f4fbcbe0be8ca83f3d23a9
Konstantinos Papagiannopoulos <76050813+kpapagiannopoulos@users.noreply.github.com>
2022-07-25 18:25:42 +0300

    Update old_files_compare.py

--- old_files_compare.py
+++ old_files_compare.py
@@ -1,9 +1,9 @@
 import pandas as pd
 col_list = ["SECTOR_ID"]
-df1 = pd.read_csv('24072022_Actix_CellRef_5GExport_Ericsson.csv', low_memory=False, usecols=col_list )
+df1 = pd.read_csv('24072022_Actix_CellRef_LTEExports_Ericsson.csv', low_memory=False, usecols=col_list )
 df2 = pd.read_csv('export_old_sectors.csv', low_memory=False, usecols=col_list)
 
-result = df1.merge(df2, on="SECTOR_ID", how="left").fillna(0).set_index("SECTOR_ID")
+result = pd.merge(df1, df2, how="inner").fillna(0).set_index("SECTOR_ID")
 
 print(result)
-result.to_csv ('5G_ERC_MISSING.csv')
+result.to_csv ('LTE_ERC_MISSING.csv')
